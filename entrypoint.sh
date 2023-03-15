6cdd89f8e091b7e9fc8b15093be72928274b2710
staylightblow8 <liudf0716@gmail.com>
2023-03-15 09:26:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# replace SS_PASSWORD, SS_METHOD  and KCP_PORT in supervisord.conf with env variable
+sed -i "s/SS_PASSWORD/$SS_PASSWORD/g" /etc/supervisor/conf.d/supervisord.conf
+sed -i "s/SS_METHOD/$SS_METHOD/g" /etc/supervisor/conf.d/supervisord.conf
+sed -i "s/KCP_PORT/$KCP_PORT/g" /etc/supervisor/conf.d/supervisord.conf
+
+# start supervisord
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
